12942ffc496a678196c42689c5d2bf91b9a5b713
Viktor Rezvantsev <geroid1735@gmail.com>
2020-02-13 11:03:21 +0300

    first commit

---
+++ SuperWeather/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SuperWeather
+//
+//  Created by vrez on 04.02.2020.
+//  Copyright © 2020 Viktor Rezvantsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
+    private let openWeatherMapKey = "4b0448e475b90f74791e1f6fe7ca5038"
+    
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var WeatherDesc: UILabel!
+    
+    @IBAction func weatherButtonTapped(_ sender: UIButton) {
+        let weather = WeatherGetter(controller: self)
+        weather.getWeather(city: "Taganrog")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let weather = WeatherGetter(controller: self)
+        weather.getWeather(city: "Taganrog")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ SuperWeather/WeatherGetter.swift
@@ -0,0 +1,71 @@
+//
+//  WeatherGetter.swift
+//  SuperWeather
+//
+//  Created by vrez on 04.02.2020.
+//  Copyright © 2020 Viktor Rezvantsev. All rights reserved.
+//
+
+import Foundation
+
+
+class WeatherGetter {
+
+    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
+    private let openWeatherMapKey = "4b0448e475b90f74791e1f6fe7ca5038"
+    private var controller: ViewController? = nil
+    init(controller: ViewController){
+        self.controller = controller
+    }
+    
+    func getWeather(city: String){
+        
+        let session = URLSession.shared
+        let weatherRequestURL = URL(string: "\(openWeatherMapBaseURL)?APPID=\(openWeatherMapKey)&lang=ru&q=\(city)&units=metric")!
+        
+        let dataTask = session.dataTask(with: weatherRequestURL){
+            (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error{
+                print("Error:\n\(error)")
+            } else {
+                if let data = data {
+                    let dataString = String(data: data, encoding: String.Encoding.utf8)
+                    print("All the weather data:\n\(dataString!)")
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                        as? NSDictionary{
+                        if let mainDictionary = jsonObj.value(forKey: "main") as? NSDictionary{
+                            if let temperature = mainDictionary.value(forKey: "temp"){
+                                DispatchQueue.main.async {
+                                   self.controller?.weatherLabel.text = " \(temperature)°C"
+                                }
+                            }
+                           
+                        } else {
+                             print("Error: unable to find temperature in dictionary")
+                     }
+                        if let timeZone = jsonObj.value(forKey: "name") as? String{
+                            if let town = timeZone as? String{
+                                                       DispatchQueue.main.async {
+                                                           self.controller?.cityLabel.text = "\(town)"
+                                                       }
+                                                    }
+                        }
+                        if let weatherDetails = jsonObj.value(forKey: "weather") as? Array<Any>{
+                            if let town2 = weatherDetails as? [[String: Any]] {
+                                                       DispatchQueue.main.async {
+                                                        self.controller?.WeatherDesc.text = "\(town2[0]["description"]!)"
+                                                        //print("Debug: \(town2[0]["description"]!)")
+                                                       }
+                                                    }
+                        }
+                    } else {
+                  print("Error: unable to convert json data")
+                 }
+                } else {
+                 print("Error: did not receive data")
+             }
+          }
+        }
+     dataTask.resume()
+    }
+}
